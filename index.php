acfb82174937667627e4219bca19a70c9c3e71c0
gabriel <levitarmouse@gmail.com>
2017-01-17 15:32:53 -0300

    Fixes integración

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 include_once './config/config.php';
 
+$composerAutoloader = __DIR__.'/vendor/autoload.php';
+if (file_exists($composerAutoloader)) {
+    require $composerAutoloader;
+    
+}
+
 $restHandler = new \levitarmouse\rest\Rest(REST_CONFIG);
 
 $restHandler->handleRequest();
---
+++ src/olx/entities/User.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace olx\entities;
+
+class User extends \levitarmouse\kiss_orm\EntityModel {
+
+}
