affd770bf09648bf56d0ac3798dd53ed28ef3bf9
gabriel <levitarmouse@gmail.com>
2017-04-24 19:50:28 -0300

    Pruebas integración !preAlpha

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -7,5 +7,5 @@ mv ./vendor/levitarmouse/kiss_rest/.htaccess .
 mkdir -p config/kissrest;
 cd ./config/kissrest;
 cp -rp ../../vendor/levitarmouse/kiss_rest/config/rest.ini .;
-cp -rp ../../vendor/levitarmouse/kiss_rest/config/config.php .;
+cp -rp ../../vendor/levitarmouse/kiss_rest/config/Bootstrap.php .;
 cp -rp ../../vendor/levitarmouse/kiss_rest/config/Autoload.php .;
