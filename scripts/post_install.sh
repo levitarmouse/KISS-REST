bd80c5b2c897b7563cf18474a2f89861ed7d5cec
gabriel <levitarmouse@gmail.com>
2017-04-25 09:46:02 -0300

    Pruebas funcionales

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -10,4 +10,4 @@ cp -rp ./vendor/levitarmouse/kiss_rest/dist.htaccess ./.htaccess;
 cd ./config/kissrest;
 cp -rp ../../vendor/levitarmouse/kiss_rest/config/rest.ini .;
 cp -rp ../../vendor/levitarmouse/kiss_rest/config/Bootstrap.php .;
-cp -rp ../../vendor/levitarmouse/kiss_rest/config/Autoload.
+cp -rp ../../vendor/levitarmouse/kiss_rest/config/Autoload.php .;
