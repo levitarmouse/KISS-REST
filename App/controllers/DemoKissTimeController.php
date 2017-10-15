3f0e39579976a2f2788360dae8a4e01c8d719e6c
gabriel <levitarmouse@gmail.com>
2017-10-14 21:22:03 -0300

    update

--- App/controllers/DemoKissTimeController.php
+++ App/controllers/DemoKissTimeController.php
@@ -5,8 +5,20 @@ namespace controllers;
 class DemoKissTimeController extends KissBaseController {
 
     public function dateTime() {
-        $dateTime = date('d-m-Y H:i:s');
 
-        return $dateTime;
+        $dateTime = date('d-m-Y H:i:s');
+        $dbTime = $this->dbTime();
+
+        return array('phpTime' => $dateTime,
+                     'dbTime'  => $dbTime);
+    }
+
+    protected function dbTime() {
+        $model = new \levitarmouse\kiss_orm\GenericEntity();
+        $query = 'select now() as dbtime';
+
+        $dbTime = $model->getMapper()->select($query);
+
+        return $dbTime[0]['dbtime'];
     }
 }
